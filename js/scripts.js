bd67b65e563095e8d4f42e4d35c990b17e0d61ae
Student <example@example.com>
2019-10-24 13:42:09 -0700

    remove branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,49 @@
 $(document).ready(function() {
-  $("button").click(function(event) {
 
-    // $("button") .show(".animal1");
-    $("#animal1").show();
-    $("#animal2").show();
-    $("#animal3").show();
+  var lionButton = $("button#animal1")
+  var elephantButton = $("button#animal2")
+  var giraffeButton = $("button#animal3")
+
+  lionButton.click(function(event) {
+    $('#lion').show();
+  event.preventDefault();
+
+
+    elephantButton.click(function(event) {
+      $('#elephant').show();
+  event.preventDefault();
+
+
+      giraffeButton.click(function(event) {
+        $('#giraffe').show();
+
+
 
   event.preventDefault();
+      })
+
+    });
+  });
 });
-});
+
+
+
+
+// $("button") .show(".animal1");
+// $("#animal1").show();
+// $("#animal2").show();
+// $("#animal3").show();
+
+// if
+// $ ("button")
+//
+// // ("button.animal1") {
+  //   // $("#lion").show()
+  // }
+  // else if ("button.animal2" === true) {
+    //
+    //   $("#elephant").show();
+    // }
+    // else if ("button.animal3" === true) {
+      //   $("girrafe").show();
+      //   }
