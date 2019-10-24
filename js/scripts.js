cb7e2a23cb330ad4bc4fa2ada5d24bc74960c9db
Student <example@example.com>
2019-10-24 11:29:04 -0700

    add 3 animal buttons which will eventually show animal info specific to the button

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("button").click(function(event) {
+
+    // $("button") .show(".animal1");
+    $("#animal1").show();
+    $("#animal2").show();
+    $("#animal3").show();
+
+  event.preventDefault();
+});
+});
